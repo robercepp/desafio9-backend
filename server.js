2d27b9c44656d4ed66bb63a0293141551072506a
Robertino <robercepp@hotmail.com>
2023-01-28 22:19:42 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+//librerías requeridas
+const express = require('express');
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+const {engine} = require('express-handlebars');
+const app = express();
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+const mariaDB = require('./options/mariaDB.js');
+const sqLite = require('./options/sqLite.js')
+
+//engine handlebars
+app.engine('hbs', engine({
+    defaultLayout: false
+}))
+
+//middlewares
+app.set("view engine", "hbs");
+app.set("views", "./views")
+app.use(express.static('public'))
+
+//servidor
+const PORT = 8080
+const connectServer = httpServer.listen(PORT, () => console.log(`Servidor http con WebSocket escuchando el puerto ${connectServer.address().port}`))
+connectServer.on("error", error => console.log(`Error en servidor ${error}`))
+
+//class
+const dBHandler = require("./classes/dbhandler.js")
+const chat = new dBHandler(sqLite.options, 'mensajes')
+const prod = new dBHandler(mariaDB.options, 'productos')
+
+//"connection" se ejecuta la primera vez que se abre una nueva conexion
+io.on('connection', async(socket) => {
+    console.log('Nuevo cliente conectado')
+    //Envio de los mensajes al cliente que se conecto
+    socket.emit('mensajes', await chat.getChat())
+    socket.emit('mensaje', await chat.getChat())
+    socket.emit('productos', await  prod.getAll())
+    socket.emit('producto', await prod.getAll())
+    socket.emit('productos-random', await prod.randomProducts())
+    //Escucho los mensajes enviados por el cliente
+    socket.on('new-message', async(data) => {
+        await chat.saveChat(data)
+        io.sockets.emit('mensaje', await chat.getChat())
+    })
+    socket.on('new-producto', async (data) => {
+        await prod.saveProduct(data)
+        io.sockets.emit('producto', await prod.getAll())
+    })
+})
+
+app.get('/api/productos-test', async(req, res) =>{
+    res.render('test', {titulo: 'Pruebas de Productos aleatorios', lista: prod.randomProducts()})
+})
+
+app.get('/', async(req, res) =>{
+    res.render('main', {titulo: 'Engine Handlebars con Websocket', lista: prod.getAll(), mensajes: chat.getAll()})
+})
